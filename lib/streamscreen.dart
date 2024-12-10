8da6a7dbe9290ef432a26ffd86f768d69e9dbda6
carlamoreno123 <carlamorenoc2005@gmail.com>
2024-12-10 11:18:14 +0100

    streams

--- lib/streamscreen.dart
+++ lib/streamscreen.dart
@@ -18,6 +18,21 @@ class _StreamScreenState extends State<StreamScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Streams')),
       body: Center(
+Future<void> getData() async {
+  String data = await fetchData();
+  print(data);
+}
+Future<String> fetchData() {
+  return Future.delayed(Duration(seconds: 3), () => "Datos recibidos");
+}
+
+
+        Stream<int> contadorStream(int max) async* {
+	for (int i = 0; i <= max; i++) {
+		await Future.delayed(Duration(seconds: 1));
+		yield i;
+	}
+}
           // child:
           ),
     );
